3d2f5eab65f463cc067664726a3cf3cc815e851c
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-05-05 10:53:03 -0400

    Custom routes for portfolio items: show, edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show, :edit]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'portfolio/:id/edit', to: 'portfolios#edit', as: 'portfolio_edit'
 
 
   get '/about-me', to: 'pages#about'
