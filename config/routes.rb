4b95e0732dea5ad1c37b05995c596da517bea8ab
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-05-05 13:12:03 -0400

    Added friendly routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolios, except: [:show, :edit]
+  resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  get 'portfolio/:id/edit', to: 'portfolios#edit', as: 'portfolio_edit'
 
 
   get '/about-me', to: 'pages#about'
