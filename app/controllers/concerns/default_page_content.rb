10dced287f31a9938e84bc2d07aa9b63d5ffa46f
sgoyet972 <scott_goyette@comcast.com>
2018-05-08 11:36:26 -0400

    Implemented partial for nav component

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -7,6 +7,6 @@ module DefaultPageContent
 
   def set_page_defaults
     @page_title = "Scott Goyette | my Portfolio"
-    @seo_keywords = "Scott Goyette portfolio"
+    @seo_keywords = "Scott Goyette portfolio Ruby Rails Ruby on Rails Python PipEnv Pip Python3 Blog"
   end
 end
