432ddc7b894a668b48d46cfae5c540aa323d00d5
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-05-26 19:17:32 -0400

    REDIS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = [
+    'https://sg-dev-portfolio.herokuapp.com',
+    'http://sg-dev-portfolio.herokuapp.com'
+  ]
+  config.action_cable.url = "wss://sg-dev-portfolio.herokuapp.com/cable"
 end
