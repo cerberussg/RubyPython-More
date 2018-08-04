6fe66e237297f7a5234d43fb3e1fb4203d74a445
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-08-03 22:33:42 -0400

    Implementing contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,16 @@
 class ContactsController < ApplicationController
 
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      redirect_to contacts_path, notice: "Email Successfully Sent"
+    else
+      redirect_to contact_path, error: "Email could not be sent"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,6 @@ Rails.application.routes.draw do
   get '/about-me', to: 'pages#about'
   get '/contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
+
+  post '/contact', to: 'contacts#create', as: 'contacts_create'
 end
