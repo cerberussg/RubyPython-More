b24c63d4ad5b47fa42d79ec8e8be70450e5dd319
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-10-11 00:23:36 -0400

    Being lazy and not setting up a login for dev

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,7 @@ class TopicsController < ApplicationController
   before_action :set_sidebar_topics
   layout 'blog'
   def index
-    @topics = Topic.all.order("title DESC")
+    @topics = Topic.all.order("title ASC")
   end
 
   def new
