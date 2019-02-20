42ffe88c566bcc8ba9c7ba729f82ea6ad41050e8
Scott Goyette <sgoyette@kerberosfoundation.com>
2019-02-20 11:33:56 -0500

    adjusted copy on about page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,6 @@ Rails.application.configure do
 
   # Devise Mailer config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.webpacker.check_yarn_integrity = false
 end
